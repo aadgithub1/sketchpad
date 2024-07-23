7a24df7e5906548635157c557f8531b1d3605a85
Aaron <152732836+aadgithub1@users.noreply.github.com>
2024-07-23 09:14:55 -0400

    Allow user to choose grid dimension. Update style.
    Added input validation to ensure grid cannot be
    larger than 100x100 for the sake of performance.

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 let container = document.querySelector('#grid-container')
+let selectDimensionsBtn = document.querySelector('button')
 const MAX_GRID_SIZE = 1000 //px
-let numRows = 16
-let numCols = 16
-let tileHeight = MAX_GRID_SIZE / numRows
-let tileWidth = MAX_GRID_SIZE / numCols
 
-for (let i = 0; i < numRows; i++){
-    let currentRow = document.createElement('div')
-    for (let j = 0; j < numCols; j++){
-        let currentTile = document.createElement('div')
-        currentTile.setAttribute('class', 'tile')
-        currentTile.style.height = `${tileHeight}px`
-        currentTile.style.width = `${tileWidth}px`
-        currentTile.addEventListener('mouseover', function(){
-            this.setAttribute('class', 'dark')
-        })
-        currentRow.appendChild(currentTile)
+function renderGrid(numRowsAndCols = 16){
+    let tileHeight = MAX_GRID_SIZE / numRowsAndCols
+    let tileWidth = MAX_GRID_SIZE / numRowsAndCols
+    for (let i = 0; i < numRowsAndCols; i++){
+        let currentRow = document.createElement('div')
+        for (let j = 0; j < numRowsAndCols; j++){
+            let currentTile = document.createElement('div')
+            currentTile.setAttribute('class', 'tile')
+            currentTile.style.height = `${tileHeight}px`
+            currentTile.style.width = `${tileWidth}px`
+            currentTile.addEventListener('mouseover', function(){
+                this.setAttribute('class', 'dark')
+            })
+            currentRow.appendChild(currentTile)
+        }
+        container.appendChild(currentRow)
     }
-    container.appendChild(currentRow)
 }
-
-
+selectDimensionsBtn.addEventListener('click', function(){
+    let newGridDimensions = 101
+    while (newGridDimensions > 100){
+        newGridDimensions = prompt('Enter the new height and width of the grid, up to 100:')
+    }
+    container.innerHTML = ''
+    renderGrid(newGridDimensions)
+})
+renderGrid()
